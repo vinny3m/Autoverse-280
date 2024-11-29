f1324db33a3fe953dd9362fda8d2568c5c613df6
Aishwarya Murahari <aishwarya26ramagiri@gmail.com>
2024-11-29 12:57:21 -0800

    added frontend test

--- ecommerce-frontend/src/App.test.js
+++ ecommerce-frontend/src/App.test.js
@@ -1,8 +1,32 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import keycloak from './config/keycloak';
+import { CartProvider } from './context/CartContext';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App component without crashing', () => {
+  render(
+    <ReactKeycloakProvider authClient={keycloak}>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </ReactKeycloakProvider>
+  );
+
+  // Verify if Navbar is present
+  expect(screen.getByRole('navigation')).toBeInTheDocument();
 });
